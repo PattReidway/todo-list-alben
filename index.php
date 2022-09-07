1e1ffa14934aad935dd9646f49fc848e41aedee8
benjamin ebran <Ebran.b@gmail.com>
2022-09-07 11:34:58 +0200

    naissance création de génies

--- index.php
+++ index.php
@@ -4,9 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="all-css/style-css.css">
     <title>Todo list panda</title>
 </head>
-<body>
+<body class="dark-template">
+    <div class="container">
+        <header class="header">
+            <h1 class="main-ttl">My Todo List</h1>
+            <nav class="main-nav">
+                <ul class="main-nav-list">
+                    <li><a class="main-nav-link active" href="index.php">+</a></li>   
+                </ul>
+            </nav>
+        </header>
+    </div>
+    <div>
+        <ul class="task-list">
+            <li class="task"><input type="checkbox" class="checkbox"/><h2>jhgflgf</h2></li>
+            <li class="task"><input type="checkbox" class="checkbox"/><h2>2315jkbg</h2></li>
+            <li class="task"><input type="checkbox" class="checkbox"/><h2>khg</h2></li>
+            <li class="task"><input type="checkbox" class="checkbox"/><h2>hchchchclkhohc</h2></li>
+            <li class="task"><input type="checkbox" class="checkbox"/><h2>khvljg</h2></li>
+        </ul>
+
+
+    </div>
     
 </body>
 </html>
