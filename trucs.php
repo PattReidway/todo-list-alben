afd1913c7b85c95f7a3ccf110dd8aac3b2f2db50
benjamin ebran <Ebran.b@gmail.com>
2022-09-08 15:41:54 +0200

    poop

---
+++ trucs.php
@@ -0,0 +1,8 @@
+<?php
+/*
+<select name="pets" class="theme-select">
+                    <option value="">--Please choose a theme--</option>
+                    <option value="dog">Dog</option><br>
+                </select>
+                    */
+ ?>                   
