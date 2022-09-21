a9add1e8d129c56b613f8562977c6943901edb57
Vanbaelinghem Alan <Vanbaelinghem.Alan@gmail.com>
2022-09-21 15:54:08 +0200

    It's working

--- database/action.php
+++ database/action.php
@@ -14,45 +14,71 @@
      catch (Exception $e) {
         die("Unable to connect to the database." . $e->getMessage());
     }
-
+//----------------------------- the task is gone
     if (isset ($_GET["id_task"])&& isset($_GET["action"])&& ($_GET["action"]==="done")){
         $query = $dbCo->prepare("UPDATE task SET done = :done  WHERE id_task = :id_task;");
         $query->execute([
             "id_task" => $_GET["id_task"],
             "done" => 1
         ]);
-    
     }
+// ------------------------------------- priority of the task
+    $query = $dbCo->prepare("SELECT priority
+    FROM task
+    WHERE id_task = :id_task;");
+    $query->execute([
+        "id_task" => $_GET["id_task"]
+    ]);
+    $resultQuery1 = $query->fetchColumn(); 
+    $descResult = ($resultQuery1+1);
+    $ascResult = ($resultQuery1-1);
+    var_dump($resultQuery1);
+    var_dump($descResult);
+    var_dump($ascResult);
+//-------------------------------------- get the max priority
+$queryMP = $dbCo->prepare("SELECT MAX(priority) AS max_prio FROM task WHERE id_users = 1;");
+$queryMP->execute();
+$MP = $queryMP->fetchColumn();
+var_dump(isset($_GET['action']) , $_GET['action'] === "down" , isset($_GET["id_task"]) , $resultQuery1 < $MP, $resultQuery1, $MP);
+        if (isset($_GET['action']) && $_GET['action'] === "down" && isset($_GET["id_task"]) && $resultQuery1 < $MP) {
 
-    if (isset($_POST['id_task']) && $_POST['action'] === 'up' && isset($_POST('id_task')){
-        $query1 = $dbCo->prepare("UPDATE tasks"
-        SET priority = priority -1
-        WHERE priority = $orderTaks AND done = 0;")";
-        $squery->execute();
-    }
+            var_dump($_GET, 1);
 
-    // foreach($_POST["priority"] as $orderTask) {
-    //     sort($orderTask)
-    //     var_dump($ordertask)
-    // }
+            $degageDown = $dbCo->prepare("UPDATE task SET priority =:priority  WHERE priority = :newPriority AND done = 0;");
+            $degageDown->execute([
+                "priority" => $resultQuery1,
+                "newPriority" => $descResult
+            ]);
 
-    $orderTask = $dbCo->prepare("UPDATE INTO 'task' ('priority') VALUES (:number)");
-    $orderTask->execute([
-        "priority" => 
-    ])
+            $sameDown = $dbCo->prepare("UPDATE task SET priority = :priority WHERE id_task =  :id_task ;");
+            $sameDown->execute([
+                "id_task" => $_GET["id_task"],
+                "priority" => $descResult
+            ]);
+            $action = "down";
+        } elseif (isset($_GET['action']) && $_GET['action'] === "up" && isset($_GET["id_task"]) && $resultQuery1 > 1) {
+            $queryMP = $dbCo->prepare("SELECT MAX(priority) AS max_prio FROM task WHERE id_users = 1;");
+            $queryMP->execute();
+            $MP = $queryMP->fetchColumn();
 
-    $orderTask = $dbCo->query("SELECT priority FROM task");
-    $orderTask->execute();
-    $resultOrder = $orderTask->fetchAll();
-    sort($resultOrder);
+                 var_dump($_GET, 2);
+            
+                 $degageDown = $dbCo->prepare("UPDATE task SET priority =:priority  WHERE priority = :newPriority AND done = 0;");
+                 $degageDown->execute([
+                     "priority" => $resultQuery1,
+                     "newPriority" => $ascResult
+                 ]);
+     
+                 $sameDown = $dbCo->prepare("UPDATE task SET priority = :priority WHERE id_task =  :id_task ;");
+                 $sameDown->execute([
+                     "id_task" => $_GET["id_task"],
+                     "priority" => $ascResult
+                 ]);
 
-    
-    // foreach($resultOrder as priorityOrder)
-
-
-    var_dump($orderTask);
-
+                $action = "up";
+         }
 
     header('Location: http://localhost/grandprojet2/');
     exit
 ?>
+
